f74e5acdd409cab463dad1bed68cf544c8cf97cd
Alie <tlduyen@toantin.org>
2018-02-01 09:40:49 +0700

    isobar

---
+++ rhythm generate/isobar/pattern/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0"
+__author__ = "Daniel Jones <http://www.erase.net/>"
+
+from automate import *
+from chance import *
+from core import *
+from fade import *
+from harmony import *
+from lsystem import *
+from markov import *
+from operator import *
+from oscillator import *
+from sequence import *
+from static import *
+from warp import *
