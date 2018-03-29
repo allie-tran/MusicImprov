55e1ac3a5649bce05c737a0eba71c508cf8bc7d4
shoqere <tlduyen@toantin.org>
2018-03-29 11:29:00 +0700

    fix bidirectional

--- model/common_model.py
+++ model/common_model.py
@@ -19,9 +19,9 @@ class GeneralNet(Model):
 	def __init__(self, input_shape, output_shape, model_name):
 		self._model_name = model_name
 		input = Input(shape=input_shape)
-		lstm1 = Bidirectional(LSTM(512, return_sequences=True)(input))
+		lstm1 = Bidirectional(LSTM(512, return_sequences=True))(input)
 		dropout = Dropout(0.3)(lstm1)
-		lstm2 = Bidirectional(LSTM(512, return_sequences=True)(dropout))
+		lstm2 = Bidirectional(LSTM(512, return_sequences=True))(dropout)
 		dropout2 = Dropout(0.3)(lstm2)
 		reshape = Reshape((output_shape[0], -1))(dropout2)
 		output = Dense(output_shape[1])(reshape)
