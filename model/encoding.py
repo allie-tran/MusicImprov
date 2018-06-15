174f536aa7405f40b0d5c415a2a0175e780742a6
shoqere <tlduyen@toantin.org>
2018-06-15 11:07:45 +0700

    Added regularization + concatenate input

--- model/encoding.py
+++ model/encoding.py
@@ -1,21 +1,22 @@
 from keras.layers import LSTM, Input, Dense
 from keras import Model
 from keras.callbacks import ModelCheckpoint
-
+from common_model import fro_norm, cust_reg
 from scripts import args
 
-class Encoder(Model):
+class Embedder(Model):
 	def __init__(self, input_shape, output_shape, model_name):
 		self._model_name = model_name
 		self._file_path = "weights/{}.hdf5".format(self._model_name)
 
 		X1 = Input(shape=input_shape)
 
-		encoder = LSTM(args.num_units, return_sequences=True, name="encoder", dropout=args.dropout)(X1)
-		decoder = LSTM(args.num_units, return_sequences=True, name="decoder", dropout=args.dropout)(encoder)
-		X2 = Dense(output_shape[1], activation='softmax', name="Dense")(decoder)
+		embedding = LSTM(args.num_units, return_sequences=True, name="encoder", dropout=args.dropout,
+		               activity_regularizer=cust_reg)(X1)
 
-		super(Encoder, self).__init__(X1, X2)
+		X2 = Dense(output_shape[1], activation='softmax', name="Dense")(embedding)
+
+		super(Embedder, self).__init__(X1, X2)
 
 		self.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
@@ -38,7 +39,7 @@ class Encoder(Model):
 		self.fit(
 			inputs,
 			outputs,
-			epochs=args.encoder_epochs,
+			epochs=args.embedder_epochs,
 			batch_size=32,
 			callbacks=callbacks_list,
 			validation_split=0.2
@@ -47,15 +48,14 @@ class Encoder(Model):
 	def load(self):
 		self.load_weights(self._file_path)
 
-	def encode(self, X):
-		if not args.encode:
+	def embed(self, X):
+		if not args.embed:
 			return X
 
-		print 'Number of layers in encoder: ', len(self.layers)
 		from keras import backend as K
 
 		# with a Sequential model
-		get_encoded = K.function([self.layers[0].input],
+		get_embedded = K.function([self.layers[0].input],
 		                                  [self.layers[1].output])
 
-		return get_encoded([X])
+		return get_embedded([X])
