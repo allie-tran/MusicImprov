7ccbca8158aacdf8432fcd63b4aafea5af437116
shoqere <tlduyen@toantin.org>
2018-06-18 23:25:56 +0700

    Fixed errors

--- model/train_utils.py
+++ model/train_utils.py
@@ -11,7 +11,7 @@ def fro_norm(w):
     return K.sqrt(K.sum(K.square(K.abs(w))))
 
 def cust_reg(w):
-	print 'Weight matrix size: ', K.int_shape(w)
+	# print 'Weight matrix size: ', K.int_shape(w)
 	m = K.dot(K.transpose(w), w) - K.eye(K.int_shape(w)[-1])
 	return fro_norm(m)
 
