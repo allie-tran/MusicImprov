2e550976ea016dca9d502d2833c8c563d9671163
shoqere <tlduyen@toantin.org>
2018-08-18 00:08:58 +0700

    remove progbar

--- model/general_model.py
+++ model/general_model.py
@@ -51,7 +51,7 @@ class GeneralModel(object):
 		early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=paras.early_stopping, verbose=0, mode='min')
 		tensorboard = TensorBoard(log_dir="logs/" + paras.exp_name + '/' + self._model_name)
 		inspect = Eval(self._output_shape)
-		callbacks_list = [ProgbarLoggerVerbose('samples'), inspect, checkpoint, early_stopping, tensorboard]
+		callbacks_list = [inspect, checkpoint, early_stopping, tensorboard]
 
 		# Train
 		history = self.fit(data, callbacks_list)
